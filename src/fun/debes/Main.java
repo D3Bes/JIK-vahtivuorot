ed35b031cce50b77ffab2ed1af971a921953eba0
D3Bes <vkirsilaa@gmail.com>
2022-04-04 17:40:07 +0300

    Bug fixes

--- src/fun/debes/Main.java
+++ src/fun/debes/Main.java
@@ -6,18 +6,22 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
+        System.out.println("Example: ma-8.45");
         while(true) {
             try {
-                File file = new File("vuorot.txt");
-                FileReader fr = new FileReader(file);
+                File listfile = new File("vuorot.txt");
+
+                FileReader fr = new FileReader(listfile);
                 BufferedReader br = new BufferedReader(fr);
-                String s;
+
+
                 Scanner sc = new Scanner(System.in);
-                System.out.println("Example: ma-8.45");
+
                 System.out.println("'q' to quit");
                 System.out.println("Give time: ");
+
+                String s;
                 String str = sc.nextLine();
-                String input = str;
 
                 while ((s = br.readLine()) != null) {
                     if (s.contains(str)) {
@@ -29,8 +33,6 @@ public class Main {
                 if(str.contains("q")) {
                     break;
                 }
-            } catch (FileNotFoundException e) {
-                e.printStackTrace();
             } catch (IOException e) {
                 e.printStackTrace();
             }
