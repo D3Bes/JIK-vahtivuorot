ab43ed02c4571da00dd53c3b6501c612b48ea01e
D3Bes <vkirsilaa@gmail.com>
2022-04-04 17:23:42 +0300

    made break command

--- src/fun/debes/Main.java
+++ src/fun/debes/Main.java
@@ -6,27 +6,34 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        try {
-            File file = new File("vuorot1.txt");
-            FileReader fr = new FileReader(file);
-            BufferedReader br = new BufferedReader(fr);
-            String s;
-            Scanner sc = new Scanner(System.in);
-            System.out.println("Example: ma-8.45");
-            System.out.println("Give time: ");
-            String str = sc.nextLine();
-            String input = str;
+        while(true) {
+            try {
+                File file = new File("vuorot.txt");
+                FileReader fr = new FileReader(file);
+                BufferedReader br = new BufferedReader(fr);
+                String s;
+                Scanner sc = new Scanner(System.in);
+                System.out.println("Example: ma-8.45");
+                System.out.println("'q' to quit");
+                System.out.println("Give time: ");
+                String str = sc.nextLine();
+                String input = str;
 
-            while ((s = br.readLine()) != null) {
-                if (s.contains(str)) {
-                    String nextline = br.readLine();
-                    System.out.println(nextline);
+                while ((s = br.readLine()) != null) {
+                    if (s.contains(str)) {
+                        String nextline = br.readLine();
+                        System.out.println(nextline);
+                        System.out.println(" ");
+                    }
                 }
+                if(str.contains("q")) {
+                    break;
+                }
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
             }
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
         }
     }
 }
